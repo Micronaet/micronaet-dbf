027ea18fa5d8c9c2a2ac37afead548167ed86bfb
nicothebrush <nicola.riolini@gmail.com>
2018-03-02 08:43:49 +0100

    remove double code for supplier import

--- dbf_edison_partner/partner.py
+++ dbf_edison_partner/partner.py
@@ -47,7 +47,7 @@ class ResPartner(orm.Model):
     _inherit = 'res.partner'
     
     def schedule_dbf_edison_partner_import(self, cr, uid, 
-            verbose_log_count=100, context=None):
+            verbose_log_count=100, supplier_start='1', context=None):
         ''' Import partner from external DBF
         '''
         # ---------------------------------------------------------------------
@@ -67,7 +67,7 @@ class ResPartner(orm.Model):
                 _logger.info('Import customer #: %s' % i)
             
             # Mapping fields:
-            ref = record['CCODCLIE']
+            ref = record['CCODCLIE']                
             name = '%s%s' % (
                 record['CDESCLIE'] or '',
                 record['CDE2CLIE'] or '',
@@ -78,9 +78,7 @@ class ResPartner(orm.Model):
                 
             data = {
                 'is_company': True,
-                'customer': True,
                 'dbf_import': True,
-                'dbf_customer_code': ref, 
                 'ref': ref,
                 'name': name,
                 'street': record['CINDIR'],
@@ -92,95 +90,47 @@ class ResPartner(orm.Model):
                 'mobile': record['CTECCLIE'],
                 'email': record['CEMAIL'], 
                 'website': record['CSITOWEB'],
-                #'prov': record['CDPROV']
-                #'codefascia': record['CFASCIA'],
-                #'codecopa': record['CCODCOPA'],
-                #'codebanca': record['CCODBANC'],
-                #'codeiva': record['CCODCIVA'],               
-                #'desdescr': record['CDESDESCR'],
-                #'desindir': record['CDESINDIR'],
-                #'descitta':  record['CDESCITTA'], 
-                #'phone2': record['CTEUCLIE'], 
-                #'telrif': record['CTELRIF'],
-                #'mesiman': record['CMESIMAN'],
-                #'costoman': record['NCOSTMAN'],
-                #'dataman': record['DDATAMAN'], 
-                #'ctipcli': record['CTIPCLIE'],
-                #'codzona': record['CCODZONA'],
-                #'ctliarse': record['CTLIARSE'],
-                #'ctliarco': record['CTLIARCO'],
-                #'nazione': record['CNAZIONE'],  
-                #'lflagcee': record['LFLAGCEE'], 
-                #'codagen': record['CCODAGEN'],
-                #'scintex': record['NSCONTEX'],
-                #'codvett': record['CCODVETT'],
-                #'codvalu': record['CCODVALU'], 
-                #'codlist': record['CCODLIST'],
-                #'contori': record['CCONTORI'],
-                #'cstato': record['CSTATO'], 
-                #'contocorr': record['CCONTCORR'],
-                #'nspesein': record['NSPESEIN'],
-                #'nspeseboll': record['NSPEBOLL'],
-                #'nspesetras': record['NSPETRAS'],
-                #'numdic': record['CNUMDIC'], 
-                #'datdic': record['DDATDIC'],
-                #'numreg': record['CNUMREG'],
-                #'datareg': record['DDATREG'],
-                #'leffrate': record['LEFFRATE'],
-                #'cfiller': record['CFILLER'], 
-                #'nqualif': record['NQUALIFI'],
-                #'datanse': record['DDATINSE'],
-                #'criferm': record['CRIFERIM'],
-                #'contratt': record['CCORTATT'],
-                #'lspesein': record['LSPESEIN'], 
-                #'lspeseboll': record['LSPEBOLL'],
-                #'lnoexpco': record['LNOEXPCO'],
-                #'nspefiss': record['NSPEFISS'],
-                #'codcinn': record['CCODCINN'],
-                #'codcine': record['CCODCINE'], 
-                #'nscofatt': record['NSCOFATT'],
-                #'nscoarfa': record['NSCOARFA'],
-                #'nstsincr': record['NSTSINCR'],
-                #'ddatoper': record['DDATOPER'],
-                #'coraoper': record['CORAOPER'], 
-                #'ccodoper': record['CCODOPER'],
-                #'ccoduten': record['CCODUTEN'],
-                #'nricmano': record['NRICMANO'],
-                #'nricspex': record['NRICSPEX'],
-                #'lpersona': record['LPERSONA'], 
-                #'cfiller1': record['CFILLER1'],
-                #'ctec2cli': record['CTEC2CLI'],
-                #'ctelrif2': record['CTELRIF2'],
-                #'codammi': record['CCODAMMI'],
-                #'codport': record['CCODPORT'], 
-                #'tipsogg': record['CTIPSOGG'],
-                #'nqualif2': record['NQUALIF2'],
-                #'nqualif3': record['NQUALIF3'],
-                #'lcalcrit': record['LCALCRIT'],
-                #'lfrzprzv': record['LFRZPRZV'], 
-                #'lfrznprz': record['LFRZNPRZ'],
-                #'cemail2': record['CEMAIL2'],
-                #'cpec': record['CPEC'],
-                #'mzona': record['MZONA'],
-                #'mnote': record['MNOTE'],
+                #CDPROV CFASCIA CCODCOPA CCODBANC CCODCIVA CDESDESCR 
+                #CDESINDIR CDESCITTA 2CTEUCLIE CTELRIF CMESIMAN NCOSTMAN 
+                #DDATAMAN CTIPCLIE CCODZONA CTLIARSE CTLIARCO CNAZIONE
+                #LFLAGCEE CCODAGEN NSCONTEX CCODVETT CCODVALU
+                #CCODLIST CCONTORI CSTATO
+                #CCONTCORR NSPESEIN NSPEBOLL NSPETRAS CNUMDIC
+                #DDATDIC CNUMREG DDATREG LEFFRATE CFILLER
+                #NQUALIFI DDATINSE CRIFERIM CCORTATT LSPESEIN
+                #LSPEBOLL LNOEXPCO NSPEFISS CCODCINN CCODCINE
+                #NSCOFATT NSCOARFA NSTSINCR DDATOPER CORAOPER
+                #CCODOPER CCODUTEN NRICMANO NRICSPEX LPERSONA
+                #CFILLER1 CTEC2CLI CTELRIF2 CCODAMMI CCODPORT
+                #CTIPSOGG NQUALIF2 3NQUALIF3 LCALCRIT LFRZPRZV
+                #LFRZNPRZ CEMAIL2 CPEC MZONA MNOTE
                 }
+            
+            if supplier_start and ref.startswith(supplier_start): # supplier
+                dbf_code = 'dbf_supplier_code'
+                data['supplier'] = True
                 
+            else: # customer
+                dbf_code = 'dbf_customer_code'
+                data['customer'] = True
+            data[dbf_code] = ref
+            
             # Search partner code:
             if vat: 
                 domain = [
                     '|', '|', 
-                    ('dbf_customer_code', '=', ref),
+                    (dbf_code, '=', ref),
                     ('name', '=', name),
                     ('vat', '=', vat),
                     ]
             else: 
                 domain = [
                     '|', 
-                    ('dbf_customer_code', '=', ref),
+                    (dbf_code, '=', ref),
                     ('name', '=', name),
                     ]
                     
-            partner_ids = self.search(cr, uid, domain, context=context)
+            partner_ids = self.search(cr, uid, domain, context=context)                
             if len(partner_ids) > 1:
                 _logger.error('Error more partner: %s (use first)' % name)
                 partner_ids = [partner_ids[0]]
@@ -193,119 +143,8 @@ class ResPartner(orm.Model):
                     _logger.warning('Remove VAT: %s' % data['vat'])
                     del(data['vat'])
                     try:
-                        self.write(cr, uid, partner_ids, data, context=context)
-                    except:
-                        _logger.error('Error data: %s' % data)
-                                                
-            else:
-                try:
-                    self.create(cr, uid, data, context=context)                
-                except:
-                    # Try to remove vat
-                    _logger.warning('Remove VAT: %s' % data['vat'])
-                    del(data['vat'])
-                    try:
-                        self.create(cr, uid, data, context=context)                
-                    except:
-                        _logger.error('Error data: %s' % data)
-
-        # ---------------------------------------------------------------------
-        #                             SUPPLIER
-        # ---------------------------------------------------------------------
-        db = company_pool.get_dbf_table(
-            cr, uid, 'TBFORN.DBF', context=context)
-            
-        i = 0
-        #XXX db.field_names
-        for record in db:
-            i += 1
-            if verbose_log_count and i % verbose_log_count == 0:
-                _logger.info('Import supplier #: %s' % i)
-            
-            # Mapping fields:
-            ref = record['CCODFORN']
-            name = '%s%s' % (
-                record['CDESFORN'] or '',
-                record['CDE2FORN'] or '',
-                )
-            vat = record['CPARTIVA']
-            if vat and vat[:1].isdigit():
-                vat = 'IT%s' % vat
-            data = {
-                'is_company': True,
-                'supplier': True,
-                'dbf_import': True,
-                'dbf_supplier_code': ref, 
-                #'ref': ref,
-                'name': name,
-                'street': record['CINDIR'],
-                'city': record['CCOMUNE'],
-                'zip': record['CCAP'],
-                'fiscalcode': record['CCODFISC'],
-                'vat': vat,
-                'phone': record['CTEAFORN'],
-                'mobile': record['CTECFORN'],
-                'email': record['CEMAIL'], 
-                'website': record['CSITOWEB'],
-                }
-                #'cprov': record['CPROV'], 'ccodaggi': record[CCODAGGI],
-                #'ddatulag': record[DDATULAG], 'cfilelis': record[CFILELIS],
-                #'cesclcar': record[CESCLCAR], 'cteuforn': record[CTEUFORN], 
-                #'ctelrif': record[CTELRIF], 'ccodprag': record[CCODPRAG],
-                #'lflagages': record[LFLAGAGES], 'nflagages': record[NFLAGAGES],
-                #'ccodbanc': record[CCODBANC], 'ccodciva': record[CCODCIVA]    
-                #'ccodcopa': record[CCODCOPA], 'nscontex': record[NSCONTEX],    
-                #'ccontori': record[CCONTORI], 'cstato': record[CSTATO],
-                #'ccontcorr': record[CCONTCORR], 'nqualifi': record[NQUALIFI],
-                #'ccodcate': record[CCODCATE], 'ddatinse': record[DDATINSE],
-                #'criferim': record[CRIFERIM], 'ccortatt': record[CCORTATT],
-                #'cversaggln': record[CVERSAGGLN], 'cfilepro': record[CFILEPRO],
-                #'cfilebar': record[CFILEBAR], 'cfilefas': record[CFILEFAS],
-                #'cfilecav': record[CFILECAV], 'ccodagen': record[CCODAGEN],
-                #'lnoexpco': record[LNOEXPCO], 'cnazione': record[CNAZIONE],
-                #'ccodcinn': record[CCODCINN], 'ccodcine': record[CCODCINE],
-                #'nstsincr': record[NSTSINCR], 'ddatoper': record[DDATOPER],
-                #'coraoper': record[CORAOPER], 'ccodoper': record[CCODOPER],
-                #'ccoduten': record[CCODUTEN], 'lpersona': record[LPERSONA]    
-                #'nqualif2': record[NQUALIF2], 'nqualif3': record[NQUALIF3],
-                #'cspediz': record[CSPEDIZ], 'ccodport': record[CCODPORT],
-                #'ccodzona': record[CCODZONA], 'cemail2': record[CEMAIL2],
-                #'cpec': record[CPEC], 'mzona': record[MZONA],
-                #'mnote': record[MNOTE], 'mnoteord': record[MNOTEORD],
-                #'mprodser': record[MPRODSER], 'mvalanno': record[MVALANNO],    
-                #'mstrateg': record[MSTRATEG], 'mesitrif': record[MESITRIF],
-                #'mrivalut': record[MRIVALUT],
-
-            # Search partner code:
-            if vat: 
-                domain = [
-                    '|', '|', 
-                    ('dbf_supplier_code', '=', ref),
-                    ('name', '=', name),
-                    ('vat', '=', vat),
-                    ]
-            else: 
-                domain = [
-                    '|', 
-                    ('dbf_supplier_code', '=', ref),
-                    ('name', '=', name),
-                    ]
-                    
-            partner_ids = self.search(cr, uid, domain, context=context)
-            
-            if len(partner_ids) > 1:
-                _logger.error('Error more partner: %s (use first)' % name)
-                partner_ids = [partner_ids[0]]
-                
-            if partner_ids:
-                try:
-                    self.write(cr, uid, partner_ids, data, context=context)
-                except:
-                    # Try to remove vat
-                    _logger.warning('Remove VAT: %s' % data['vat'])
-                    del(data['vat'])
-                    try:
-                        self.write(cr, uid, partner_ids, data, context=context)
+                        self.write(
+                            cr, uid, partner_ids, data, context=context)
                     except:
                         _logger.error('Error data: %s' % data)
                                                 
