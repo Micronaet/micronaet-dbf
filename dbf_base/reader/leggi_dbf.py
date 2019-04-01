86b1e59b3a803bea488a0b0635fbab26a73ed985
nicothebrush <nicola.riolini@gmail.com>
2019-04-01 09:27:13 +0200

    generic bugfix (fast message)

---
+++ dbf_base/reader/leggi_dbf.py
@@ -0,0 +1,23 @@
+from dbfread import DBF
+
+dbf_root_path = '.'
+table = 'ARTICO.dbf'
+filename = os.path.join(dbf_root_path, table)
+
+# Read parameter:    
+dbf_ignorecase = True
+dbf_memofile = False
+dbf_encoding = 'LATIN'
+
+db = DBF(
+    filename, 
+    ignorecase=dbf_ignorecase,
+    ignore_missing_memofile=dbf_memofile,
+    encoding=dbf_encoding,
+    )
+    
+for record in db:
+    print record
+
+
+
