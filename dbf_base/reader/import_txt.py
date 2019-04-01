2f9c1dc9ee46acc22eaa8190466770f3d25f841a
nicothebrush <nicola.riolini@gmail.com>
2019-04-01 11:32:44 +0200

    generic bugfix (fast message)

--- dbf_base/reader/import_txt.py
+++ dbf_base/reader/import_txt.py
@@ -67,7 +67,7 @@ for root, folders, files in os.walk(path):
                      print '   Jumped', row
                      continue
 
-                 lst_price = float(row[79:96].replace(' ', '')[1:])
+                 lst_price = float(row[79:96].replace(' ', '').replace(',', '.')[1:])
 
                  print 'Used', row
                  if not product_ids:
